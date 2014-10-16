4d92621d32b828a6d9212d21dd2893adca594e5e
jitterxx <sergey@reshim.com>
2014-10-16 16:18:11 +0400

    add validators

--- service/service/views.py
+++ service/service/views.py
@@ -1,12 +1,58 @@
 """ Cornice services.
 """
+import os
+import binascii
+import json
+
+from webob import Response, exc
 from cornice import Service
 
+users = Service(name='users', path='/users', description="Users")
+_USERS = {}
 
-hello = Service(name='hello', path='/', description="Simplest app")
+#
+# Helpers
+#
+def _create_token():
+    return binascii.b2a_hex(os.urandom(20))
 
 
-@hello.get()
-def get_info(request):
-    """Returns Hello in JSON."""
-    return {'Hello': 'World111'}
+class _401(exc.HTTPError):
+    def __init__(self, msg='Unauthorized'):
+        body = {'status': 401, 'message': msg}
+        Response.__init__(self, json.dumps(body))
+        self.status = 401
+        self.content_type = 'application/json'
+
+
+def valid_token(request):
+    header = 'X-Messaging-Token'
+    token = request.headers.get(header)
+    if token is None:
+        raise _401()
+
+    token = token.split('-')
+    if len(token) != 2:
+        raise _401()
+
+    user, token = token
+
+    valid = user in _USERS and _USERS[user] == token
+    if not valid:
+        raise _401()
+
+    request.validated['user'] = user
+
+
+def unique(request):
+    name = request.body
+    if name in _USERS:
+        request.errors.add('url', 'name', 'This user exists!')
+    else:
+        user = {'name': name, 'token': _create_token()}
+        request.validated['user'] = user
+
+
+#
+# Services
+#
