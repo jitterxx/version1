be3c546b786b2721e093269d6190526f65203dce
jitterxx home <sergey@reshim.com>
2014-10-25 16:48:37 +0400

    Getting Email are ready Text need to be normalized by pymorph2

--- test/imap.py
+++ test/imap.py
@@ -7,15 +7,19 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import chardet
 import re
+import pymorphy2
 
 
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+
+debug = True
+
 def get_text(data):
     #Извлекаем из html сообщения только текст
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data,from_encoding="utf8")
 
     # Содержимое ссылок заменяем на LINK
     tag = soup.new_tag(soup,"b")
@@ -28,10 +32,8 @@ def get_text(data):
         link.replaceWith(tag)
     
     text = soup.get_text()
-    
     text = strip_text(text)
     
-
     return text
 
 def strip_text(data):
@@ -41,163 +43,142 @@ def strip_text(data):
     #Multiple spaces in one
     #text = re.sub(r'\s{2:}',r' ',data,re.I|re.U|re.M)
     #text = re.sub(u'\s+',u' ', text,re.I|re.U|re.M)
-    text = data.replace('\\n',' ')
-    text = text.replace('\\t',' ')    
-    text = text.replace('\\r',' ')
+    #text = data.replace('\\n',' ')
+    #text = text.replace('\\t',' ')    
+    text = data.replace('\\W',' ')
+    
     text = ' '.join(text.split())
     
     return text
 
-debug = True
 
-server = "imap.gmail.com"
-port = "993"
-login = "sergey@reshim.com"
-password = ""
-date_after =  "23-oct-2014"
-date_before = "21-oct-2014"
-
-try:
-    M = imaplib.IMAP4_SSL(server)
-except: 
-    print 'Connection problem!', sys.exc_info()[0]
+def get_emails():
+    server = "imap.gmail.com"
+    port = "993"
+    login = "sergey@reshim.com"
+    password = ""
+    date_after =  "21-oct-2014"
+    date_before = "21-oct-2014"
     
+    try:
+        M = imaplib.IMAP4_SSL(server)
+    except: 
+        print 'Connection problem!', sys.exc_info()[0]
         
-
-print M.PROTOCOL_VERSION
-password = raw_input('Password:')
-
-M.login(login,password)
-M.select()
-
-
-typ, data = M.search('UTF8','SINCE',date_after)
-print typ
-#print data
-
-
-#typ1, data1 = M.thread('REFERENCES','UTF-8','ALL')
-#print typ1
-#print data1
-
-s = {}
-
-
-for num in data[0].split():
-    typ, data = M.fetch(num, '(RFC822)')
-
-    msg = email.message_from_string(data[0][1])
-    
-    
-    msg_data = {}
-    for n,m in msg.items():
-        k = ''
-        if debug:
-            #print m
-            pass
-
-        m = strip_text(m)
-        if debug:
-            #print m
-            pass
-        
-        for h in email.header.decode_header(m):
             
-            k = ' '.join((k,h[0]))            
-            if not (h[1] == None):            
-                #Делаем перекодировку в UTF8
-                k = k.decode(h[1]).encode('utf8')
+    
+    print M.PROTOCOL_VERSION
+    password = raw_input('Password:')
+    
+    M.login(login,password)
+    M.select()
+    
+    
+    typ, data = M.search('UTF8','SINCE',date_after)
+    print typ
+
+    s = {}
+    
+    
+    for num in data[0].split():
+        typ, data = M.fetch(num, '(RFC822)')
+    
+        msg = email.message_from_string(data[0][1])
+        
+        
+        msg_data = {}
+        for n,m in msg.items():
+            k = ''
+    
+            if debug:
+                #print m
+                pass
+    
+            m = strip_text(m)
+            if debug and num == '6297':
+                #print m
+                pass
+            
+            for h in email.header.decode_header(m):
+                
+                k = ' '.join((k,h[0]))            
+                if not (h[1] == None):            
+                    #Делаем перекодировку в UTF8
+                    k = k.decode(h[1]).encode('utf8')
+                else:
+                    #Проверяем что строка корректно перекодирована
+                    if not (chardet.detect(k)['encoding'] == 'UTF-8'):
+                        k = k.decode(chardet.detect(k)['encoding']).encode('utf8')
+                
+            if n in msg_data.keys():
+                msg_data[n] = msg_data[n] + k
             else:
-                #Проверяем что строка корректно перекодирована
-                if not (chardet.detect(k)['encoding'] == 'UTF-8'):
-                    k = k.decode(chardet.detect(k)['encoding']).encode('utf8')
-            
-        if n in msg_data.keys():
-            msg_data[n] = msg_data[n] + k
+                msg_data[n] = k
+        
+        if debug:
+            #print msg_data['From']
+            pass
+    
+        
+        msg_data['Text'] = ''
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() == "text/plain":
+                    html = unicode(part.get_payload(decode=True),
+                                   part.get_content_charset(),
+                                   'replace').encode('utf8','replace')
+                    msg_data['Text'] +=  get_text(html)
+                elif part.get_content_type() == "text/html":
+                    html = unicode(part.get_payload(decode=True),
+                                   part.get_content_charset(),
+                                   'replace').encode('utf8','replace')
+                    msg_data['Text'] +=  get_text(html)
+    
         else:
-            msg_data[n] = k
-    
-    if debug:
-        #print msg_data['From']
-        pass
-
-    
-    msg_data['Text'] = ''
-    if msg.is_multipart():
-        for part in msg.walk():
-            if part.get_content_type() == "text/plain":
-                html = unicode(part.get_payload(decode=True),
-                               part.get_content_charset(),
+            if msg.get_content_type() == "text/plain":
+                html = unicode(msg.get_payload(decode=True),
+                               msg.get_content_charset(),
                                'replace').encode('utf8','replace')
                 msg_data['Text'] +=  get_text(html)
-            elif part.get_content_type() == "text/html":
-                html = unicode(part.get_payload(decode=True),
-                               part.get_content_charset(),
+            elif msg.get_content_type() == "text/html":
+                html = unicode(msg.get_payload(decode=True),
+                               msg.get_content_charset(),
                                'replace').encode('utf8','replace')
                 msg_data['Text'] +=  get_text(html)
+            #print 'HTML:',html
+            #print 'TEXT:',msg_data['Text']       
+        
+        
+        s[num] = pd.Series(msg_data.values(),msg_data.keys())
 
-    else:
-        if msg.get_content_type() == "text/plain":
-            html = unicode(msg.get_payload(decode=True),
-                           msg.get_content_charset(),
-                           'replace').encode('utf8','replace')
-            msg_data['Text'] +=  get_text(html)
-        elif msg.get_content_type() == "text/html":
-            html = unicode(msg.get_payload(decode=True),
-                           msg.get_content_charset(),
-                           'replace').encode('utf8','replace')
-            msg_data['Text'] +=  get_text(html)
-        #print 'HTML:',html
-        #print 'TEXT:',msg_data['Text']       
-    
-    
-    s[num] = pd.Series(msg_data.values(),msg_data.keys())
+    M.close()
+    M.logout()
 
+    return pd.DataFrame(s)
 
-email_df = pd.DataFrame(s)
-
-#pd.set_option('display.max_colwidth',1000)
-
-#print email_df.loc['To']
-#print email_df.loc['From']
-#print email_df.loc['Subject']
-print email_df.loc['Text']
-print '-------------------'
-#print email_df.loc['Text'].split('\\W*')
-
-
-
-#print email_df.loc['References']
-#st = email_df['6286']['From']
-#print st
-
-#typ, data = M.fetch('6286', '(RFC822)')
-#msg = email.message_from_string(data[0][1])
-#print msg['From']
-
-
-#st = re.sub(u'[\n\r\t]',' ',st)
-#print email.header.decode_header(st)
-
-
-#print email_df.columns
-    
-    #print msg['From']
-    #print msg['Date']    
-    #print msg['Subject']
-    #print msg['References']
-    #print msg.keys()
-
-    
-    
-    
-    #print 'Message %s\n%s\n' % (num, data[0][1])
-M.close()
-M.logout()
+"""
+email_df = get_emails()
 
 f2 = open("email_df", 'w')
-
 email_df.to_json(f2,orient='columns')
+f2.close()
+"""
+
+f2 = open("email_df", 'r')
+email_df = pd.read_json(f2)
+
+pd.set_option('display.max_colwidth',1000)
+
+for each in email_df.loc['Text']:
+    print each.encode('utf8')
+    print '\n--------------------------\n'
+
+
+morph = pymorphy2.MorphAnalyzer()
+
+print morph.parse(u'приветствую')[0].normal_form
+
+
 
 
 f2.close()
